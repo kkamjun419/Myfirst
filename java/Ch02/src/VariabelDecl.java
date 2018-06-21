6fba1381a0c048db60e225388d2dfbaf7671deb4
jihoon(school) <kkamjun419@gmail.com>
2018-06-21 09:49:29 +0900

    자바수업

---
+++ java/Ch02/src/VariabelDecl.java
@@ -0,0 +1,14 @@
+
+public class VariabelDecl {
+
+	public static void main(String[] args) {
+		double num1, num2;
+		double result;
+		num1 = 1.0000001;
+		num2 = 2.0000001;
+		result = num1 + num2;
+		System.out.println(result);
+
+	}
+
+}
---
+++ java/ch01/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+class FirstJavaProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Welcome to Java1");
+		System.out.print("First Java program2");
+	}
+}
